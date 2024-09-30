be96ff6db920f955ed220f90456ba80efc99d632
Marlon Campos <161335070+marlonch2@users.noreply.github.com>
2024-09-30 18:03:03 -0300

    Delete projetofaculdade directory

--- projetofaculdade/src/sistemabancario/MovimentacaoBancaria.java
+++
@@ -1,7 +0,0 @@
-package sistemabancario;
-
-public interface MovimentacaoBancaria {
-
-	abstract void depositar(double valor);
-	abstract void sacar(double valor) throws MinhaExcecaoException, SaldoInsuficienteException;
-}
